58717b288c5fcffbec92ce34f08e67ba3367c62b
adplt <atriadplt@gmail.com>
2019-01-08 14:11:59 +0700

    add for call API (GET Only)

---
+++ myCtrl.js
@@ -0,0 +1,6 @@
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+
+app.controller('myCtrl', function($scope) {
+  $scope.firstName = 'John';
+  $scope.lastName= 'Doe';
+});
